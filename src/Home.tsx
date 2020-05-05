5001954395650dd075f9723ee327ed2d57b810d2
Yiyi Sun <yiyisun@gmail.com>
2020-05-05 13:57:49 -0400

    initialize project

---
+++ src/Home.tsx
@@ -0,0 +1,11 @@
+import { app, Component } from 'apprun';
+
+export default class HomeComponent extends Component {
+  state = 'Home';
+
+  view = (state) => <div>
+    <h1>{state}</h1>
+  </div>;
+  
+}
+
